80a005647b3354a1b020ef4c65d11170e5c46bf6
ANITHA JASMINE <105966987+Anithajas30@users.noreply.github.com>
2024-05-27 11:33:47 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,19 @@
+const header =document.querySelector("header");
+
+window.addEventListener("scroll",function() {
+header.classList.toggle("sticky",window.scrollY > 120);
+
+});
+
+let  menu=document.querySelector('#menu-icon');
+let navlist= document.querySelector('.navlist');
+
+menu.onClick=() =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+};
+
+window.onscroll=() =>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
